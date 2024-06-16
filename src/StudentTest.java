4794202061f7579bd34f204eaa1a0db12c97f24f
fless <fless@inbox.ru>
2024-06-16 13:00:26 +0300

    Added function overloaded construction

--- src/StudentTest.java
+++ src/StudentTest.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
 class Student {
+    Student () {}
+    Student (int numberOfStudentTick2, String name2, String surname2, int yearOfLearn2,String course2, float money){
+        numberOfStudentTick = numberOfStudentTick2;
+        name =name2;
+        surname = surname2;
+        yearOfLearn = yearOfLearn2;
+        course =course2;
+    }
 
+    Student (int numberOfStudentTick3, String name3, String surname3, String course3) {
+        this (numberOfStudentTick3, name3, surname3, 0,course3, 0.0F);
+    }
     int numberOfStudentTick;
     String name;
     String surname;
