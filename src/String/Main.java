d2bed9cb4b3cc9cc2473e65dbda2af7fd52d2663
Zl0yR0k <Zl0yR0k@mail.ru>
2025-08-18 18:21:04 +0300

    Add branche and class for work with String function

---
+++ src/String/Main.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Main {
+    public static void main(String[] args) {
+        StringHelloWorld sh = new StringHelloWorld();
+        System.out.println(sh.s3);
+    }
+
+
+}
---
+++ src/String/StringHelloWorld.java
@@ -0,0 +1,7 @@
+package String;
+
+public class StringHelloWorld {
+    String s1 = "Hello world!";
+    String s2 = "Urassssssseee!";
+    String s3 = s1.concat(s2).strip().replace("Urassssssseee", "Урааа").substring(0,11);
+}
