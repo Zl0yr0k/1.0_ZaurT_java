6fc7a7ab8c25cba50c8b5f1474a31003fe49f51f
Zl0yR0k <Zl0yR0k@mail.ru>
2025-08-21 20:16:27 +0300

    Solve problem with String type. Not used Char type

--- src/String/Rubai.java
+++ src/String/Rubai.java
@@ -3,21 +3,24 @@ package String;
 public class Rubai {
     String r1 = "Чтоб мудро жизнь прожить, знать надобно не мало Два важных правила запомни для начала:Ты"+
             " лучше голодай, чем что попало есть,И лучше будь один, чем вместе с кем попало";
-    String r2 = r1.toUpperCase();
-    char c1;
-    char c2;
+    String upper;
+    String lower;
+
 
 
      void toPrint () {
         for (int i = 0; i < r1.length(); i++ ) {
-            c1 = r1.charAt(i);
-            c2 = r2.charAt(i);
-            if (c1 != ',' && c1 != ':' && c1 != ' ' &&  c1==c2 ) {
+            lower = String.valueOf(r1.charAt(i));
+            upper = String.valueOf(r1.charAt(i)).toUpperCase();
+            if (lower.equals(" ") || lower.equals(":") || lower.equals(",")) {
+                System.out.print(lower);
+                continue;
+            } else if ( lower.equals(upper)) {
                 System.out.println();
-                System.out.print(c1);
+                System.out.print(lower);
                 continue;
             }
-            System.out.print(c1);
+            System.out.print(lower);
         }
          System.out.println();
     }
