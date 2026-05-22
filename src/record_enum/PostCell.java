a3b36c07ed51063f8ad63edfbc2354de63114c0f
Zl0yR0k <Zl0yR0k@mail.ru>
2026-05-22 19:09:38 +0300

    fix: nullPointerExceprion in class Postomat method putShipment

--- src/record_enum/PostCell.java
+++ src/record_enum/PostCell.java
@@ -2,62 +2,65 @@ package record_enum;
 
 public class PostCell {
 
-    Dimensions sizeCell ;
+    Dimensions sizeCell;
     StatusCell statusCell;
     boolean parcel = true;
     Shipment shipmentInMassive = null;
 
-    public PostCell(double length, double width, double height){
-        sizeCell = new Dimensions(length,width,height);
+    public PostCell(double length, double width, double height) {
+        sizeCell = new Dimensions(length, width, height);
     }
-    public PostCell (double length, double width, double height, StatusCell statusCell){
-        this (length,width,height);
+
+    public PostCell(double length, double width, double height, StatusCell statusCell) {
+        this(length, width, height);
         this.statusCell = statusCell;
     }
 
-    boolean hasShipment () {
+    boolean hasShipment() {
         if (parcel == false) {
             System.out.println("Ячейка занята");
             return false;
-        }else {
+        } else {
             System.out.println("Ячейка пустая");
             return true;
         }
     }
 
-    private boolean checkDimensionsCellAndShipment (Shipment shipment) {
+    private boolean checkDimensionsCellAndShipment(Shipment shipment) {
         boolean checkSize = shipment.dimensions().length() <= sizeCell.length() &&
                 shipment.dimensions().width() <= sizeCell.width() &&
                 shipment.dimensions().height() <= sizeCell.height();
-        if (checkSize){
+        if (checkSize) {
             System.out.println("Размеры подходят");
             return true;
         } else {
             System.out.println("Размеры не подходят");
             return false;
-
-
         }
-
     }
+
     boolean canAcceptShipment(Shipment shipment) {
-        if (statusCell==StatusCell.WORK && hasShipment() && checkDimensionsCellAndShipment(shipment) ) {
+
+        if (statusCell == StatusCell.WORK && hasShipment() && checkDimensionsCellAndShipment(shipment)) {
             shipmentInMassive = shipment;
-            System.out.println("Посылка размещена в ячейке" );
+            System.out.println("Посылка размещена в ячейке");
             parcel = false;
             return true;
         } else {
             System.out.println("Посылка не размещена");
             return false;
         }
+    }
+
+    private void extractShipment() {
 
     }
-    private void  extractShipment() {
 
-    }
     private void turnMaintenanceMode() {
 
     }
-    private  void turnWorkingMode(){}
+
+    private void turnWorkingMode() {
+    }
 
 }
--- src/record_enum/Postamat.java
+++ src/record_enum/Postamat.java
@@ -2,22 +2,30 @@ package record_enum;
 
 public class Postamat {
 
-    PostCell[] cells = new PostCell[20];
+    PostCell[] cells = new PostCell[10];
 
     {
         cells[0] = new PostCell(20, 10, 30, StatusCell.WORK);
         cells[1] = new PostCell(25, 20, 40, StatusCell.WORK);
-        cells[3] = new PostCell(100, 25, 35, StatusCell.WORK);
+        cells[3] = new PostCell(100, 35, 35, StatusCell.WORK);
         cells[3] = new PostCell(10, 25, 35, StatusCell.WORK);
-        cells[4] = new PostCell(100, 25, 35, StatusCell.WORK);
+        cells[4] = new PostCell(100, 35, 35, StatusCell.WORK);
         cells[6] = new PostCell(100, 25, 35, StatusCell.AT_MAINTENANCE);
-        cells[19] = new PostCell(100, 25, 35, StatusCell.AT_MAINTENANCE);
+        cells[9] = new PostCell(100, 25, 35, StatusCell.AT_MAINTENANCE);
     }
 
     void putShipment(Shipment shipment) {
-        for (PostCell ps : cells){
-            ps.canAcceptShipment(shipment);
+
+        for (PostCell ps : cells) {
+            try {
+                ps.canAcceptShipment(shipment);
+                System.out.println("___________");
+            } catch (NullPointerException e) {
+                System.out.println("Ячейка не существует");
+                System.out.println("_____________________");
+            }
         }
+
     }
 
     void getShipment(int numberOfCell) {
