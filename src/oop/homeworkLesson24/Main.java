57356b4aa5a4b2f5a1cfe1346805dae72842e199
Valery Ost <v.k.ostapenko55@gmail.com>
2026-06-23 17:15:57 +0300

    fix: Add PSVM in Main class Lesson 24

--- src/oop/homeworkLesson24/Main.java
+++ src/oop/homeworkLesson24/Main.java
@@ -1,6 +1,8 @@
 package oop.homeworkLesson24;
 
 public class Main {
+    public static void main(String[] args) {
+                
         Mechenosec mech = new Mechenosec("Tihon");
         mech.eat();
         mech.swim();
@@ -33,9 +35,9 @@ public class Main {
         Animal animal1 = new Penguin("Zs");
         Speakable speakable1 = new Penguin("Wd");
 
-        Penguin penguin2 = (Penguin)bird1;
-        Penguin penguin3 = (Penguin)animal1;
-        Penguin penguin4 = (Penguin)speakable1;
+        Penguin penguin2 = (Penguin) bird1;
+        Penguin penguin3 = (Penguin) animal1;
+        Penguin penguin4 = (Penguin) speakable1;
         ((Penguin) bird1).abc2();
         ((Penguin) animal1).abc2();
         ((Penguin) speakable1).abc2();
@@ -44,9 +46,9 @@ public class Main {
         penguin4.abc2();
 
 
-        Bird bird2 = (Bird)animal1;
-        Bird bird3 = (Bird)speakable1;
-        System.out.println( bird2.name);
+        Bird bird2 = (Bird) animal1;
+        Bird bird3 = (Bird) speakable1;
+        System.out.println(bird2.name);
         bird2.eat();
         ((Penguin) animal1).fly();
         bird3.speak();
@@ -59,19 +61,17 @@ public class Main {
         speakable3.speak();
 
 
-
-
         Object[] speakables = new Object[]{
-               penguin1, penguin2,penguin3,penguin4, bird1,bird2,bird3,speakable1,speakable2,speakable3,speakable4,animal1
+                penguin1, penguin2, penguin3, penguin4, bird1, bird2, bird3, speakable1, speakable2, speakable3, speakable4, animal1
         };
-        Animal [] animals = new Animal[] {};
-        int z =0;
+        Animal[] animals = new Animal[]{};
+        int z = 0;
 
-        for (Object o : speakables){
-             if(o instanceof Penguin){
-                  System.out.println("Is penguin " + z);
-                  z++;
-             }
+        for (Object o : speakables) {
+            if (o instanceof Penguin) {
+                System.out.println("Is penguin " + z);
+                z++;
+            }
         }
     }
 }
